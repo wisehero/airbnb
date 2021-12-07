7aff04df6bad58a7f1515377b491a26152f7c95e
김지웅 <godjw96@gmail.com>
2021-12-07 16:47:46 +0900

    #3.5 UserAdmin + CustomAdmin

--- users/admin.py
+++ users/admin.py
@@ -1,13 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 # Register your models here.
 
 
 @admin.register(models.User)  # 관리자 등록
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):
 
     """ CUSTOM USER ADMIN """
-    list_display = ("username", "email", "gender",
-                    "language", "currency", "superhost")
-    list_filter = ("language", "currency", "superhost")
+    fieldsets = UserAdmin.fieldsets + (
+        ("Custom Profile", {
+            "fields": (
+                "avatar",
+                "gender",
+                "bio",
+                "birthdate",
+                "language",
+                "currency",
+                "superhost"
+            ),
+        }),
+    )
