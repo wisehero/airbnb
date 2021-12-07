4e0d3ed822672db1f8975f2e7d02756168c39535
김지웅 <godjw96@gmail.com>
2021-12-07 16:23:43 +0900

    #3.3 Finishing User Model

--- users/models.py
+++ users/models.py
@@ -18,7 +18,26 @@ class User(AbstractUser):
         (GENDER_OTHER, "Other")
     )
 
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kr"
+
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENGLISH, "English"),
+        (LANGUAGE_KOREAN, "Korean")
+    )
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
+
     avatar = models.ImageField(null=True, blank=True)
     gender = models.CharField(choices=GENDER_CHOICES,
                               max_length=10, null=True, blank=True)  # CharField는 한 줄 입력창
     bio = models.TextField(default="")  # TextField는 여러 줄 입력 창
+    birthdate = models.DateField(null=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES, max_length=2, null=True, blank=True)
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES, max_length=3, null=True, blank=True)
+    superhost = models.BooleanField(default=False)
