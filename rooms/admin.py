05d7a8546284a50f53c065f336da57b091b15b80
김지웅 <godjw96@gmail.com>
2021-12-09 07:27:28 +0900

    #6.0 Room Admin Panel

--- rooms/admin.py
+++ rooms/admin.py
@@ -10,7 +10,23 @@ class ItemAdmin(admin.ModelAdmin):
 
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
-    pass
+        list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+    )
+
+    list_filter = ("instant_book", "city", "country")
+
+    search_fields = ("=city", "^host__username")
 
 
 @admin.register(models.Photo)
