59fabc9e7db511c2e5b01eac382f73f0247bd31b
김지웅 <godjw96@gmail.com>
2021-12-09 07:50:46 +0900

    #6.2 Custom Admin Functions

--- rooms/admin.py
+++ rooms/admin.py
@@ -39,6 +39,7 @@ class RoomAdmin(admin.ModelAdmin):
         "check_in",
         "check_out",
         "instant_book",
+        "count_amenities"
     )
 
     list_filter = ("instant_book", "city", "country")
@@ -46,6 +47,9 @@ class RoomAdmin(admin.ModelAdmin):
     search_fields = ("=city", "^host__username")
 
     filter_horizontal = ("amenities", "facilities", "house_rules")
+
+    def count_amenities(self, obj):
+        return obj.amenities.count()
 
 
 @admin.register(models.Photo)
