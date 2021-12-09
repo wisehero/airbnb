e5641fae4756e54183fb43db66e65cfc9112f8b4
김지웅 <godjw96@gmail.com>
2021-12-10 00:41:14 +0900

    #8.6 raw_ids and Inline Admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -13,21 +13,26 @@ class ItemAdmin(admin.ModelAdmin):
         return obj.rooms.count()
 
 
+class PhotoInline(admin.TabularInline):
+
+    model = models.Photo
+
+
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
+
+    inlines = (PhotoInline,)
 
     fieldsets = (
         (
             "Basic Info",
-            {"fields": ("name", "description", "country", "address", "price")},
+            {"fields": ("name", "description", "country",
+                        "city", "address", "price")},
         ),
         ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
         ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths")}),
         ("More About the Space",
-         {
-             "classes": ("collapse",),
-             "fields": ("amenities", "facilities", "house_rules"),
-         },
+         {"fields": ("amenities", "facilities", "house_rules"), },
          ),
         ("Last Details", {"fields": ("host",)}),
     )
@@ -62,7 +67,7 @@ class RoomAdmin(admin.ModelAdmin):
         return obj.photos.count()
 
 
-@admin.register(models.Photo)
+@ admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
 
     list_display = ("__str__", "get_thumbnail")
